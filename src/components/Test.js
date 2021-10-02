a37e0124e96e394bf0e52af8f5e23e94da3531a7
AlejoVinluan <alejo.vinluan146@gmail.com>
2021-10-02 05:06:06 -0500

    Enables video conference
    Video conferences are currently located in ./Test.js, need to implement a dedicated page for ./Call.js

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Call from './Call'
 
 function Test(){
     return(
         <div className = "bg-red-200">
-            Hello World
+            <Call />
         </div>
     )
 }

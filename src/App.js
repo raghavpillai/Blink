eb637d65ec81cdbd6b13628f81d54e750d7c61cf
George <georgezhu211@gmail.com>
2021-10-02 15:21:14 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
- 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import Chat from './components/Chat';
 import Home from './components/Home';
 import Login from './components/Login';
 import Organizations from './components/Organizations'
 import Test from './components/Test.js'
-// import Error from './components/Error';
-// import Navigation from './components/Navigation';
- 
-class App extends Component {
-  render() {
-    return (      
-       <BrowserRouter>
-        <div>
-          {/* <Navigation /> */}
-            <Switch>
-             <Route path="/" component={Home} exact/>
-             <Route path="/login" component={Login} exact/>
-             <Route path="/organizations" component={Organizations} exact/>
-             <Route path="/chat" component={Chat} exact/>
-             <Route path="/test" component={Test} exact/>
-            <Route component={Error}/>
-           </Switch>
-        </div> 
-      </BrowserRouter>
-    );
-  }
+import Messaging from './messaging'
+
+export default function App() {
+  return (
+    <Router>
+      <div className="flex text-2xl justify-between border-2 border-black border-solid">
+        <div className="border-2 p-3 mx-2 justify-self-start">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="flex">
+          <div className="border-2 p-3 mx-2">
+            <Link to="/chat">Chat</Link>
+          </div>
+          <div className="border-2 p-3 mx-2">
+            <Link to="/organizations">Organizations</Link>
+          </div>
+          <div className="border-2 p-3 mx-2">
+            <Link to="/test">Test</Link>
+          </div>
+        </div>
+
+      </div>
+
+      <hr />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/chat">
+          <Chat />
+        </Route>
+        <Route path="/organizations">
+          <Organizations />
+        </Route>
+        <Route path="/organizations">
+          <Test />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
- 
-export default App;

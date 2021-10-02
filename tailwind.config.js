54ca87fad56288041624191713fa15f6fba6ec62
nam-t24 <namtruong831@gmail.com>
2021-10-02 13:55:33 -0500

    home page update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
       inset: {
         '4/10': '40%',
       },
+      height: {
+        '0.7screen':'70vh',
+      },
+      animate: {
+        fadeIn: "fadeIn 1s ease-in forwards"
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      },
     },
   },
   variants: {

95af69eccba28ce10cbc00a636ec31c4bb5d76c2
nam-t24 <namtruong831@gmail.com>
2021-10-02 02:04:09 -0500

    home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,14 @@ import React from "react";
 
 function Home(){
     return(
-        <div className = "h-screen bg-red-200 flex justify-center items-center"> 
-            <div className = "">
+        <div className = "h-screen bg-red-200"> 
+            <div className = "absolute top-1/2 left-1/2 transform -translate-x-1/2">
                 Blink Logo
             </div>
+            <div className = "absolute left-1/2 top-3/4 transform -translate-x-1/2">
+                Test
+            </div>
+            
         </div>
     )
 }
